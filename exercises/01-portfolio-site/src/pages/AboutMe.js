8bd6606a14590fe6d9b4f1122ee97821e46f295d
bcree11 <b_cree@yahoo.com>
2019-01-28 15:04:40 -0800

    portfolio

--- exercises/01-portfolio-site/src/pages/AboutMe.js
+++ exercises/01-portfolio-site/src/pages/AboutMe.js
@@ -5,7 +5,7 @@ export default class AboutMe extends Component {
     return (
       <div>
         <h1>About Me</h1>
-        <h3>My name is Jonathan Corea</h3>
+        <h2>Jonathan Corea</h2>
         <h3>My phone number is (661) 568-8744</h3>
       </div>
     );
